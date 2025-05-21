88bdfe10d307956e3215efd0611b4819a14d1bab
Atima <atimarity@gmail.com>
2025-05-22 02:30:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ import json
 import shutil
 from datetime import timedelta
 
+from commands import currency
+currency.cur(bot)
+
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
@@ -164,47 +167,5 @@ async def mute(ctx, member: discord.Member = None, duration: str = "None", *, re
 def save_balance():
     with open("balance.json", "r") as f:
         json.dump("balance.json", f, indent=4)
-
-@bot.command()
-async def bal(ctx, member: discord.Member = None):
-
-    member = member or ctx.author
-    user_id = str(member.id)
-
-    if not os.path.exists(BALANCE_FILE):
-        with open(BALANCE_FILE, "w") as f:
-            json.dump({}, f)
-
-    with open(BALANCE_FILE, "r") as f:
-        balance = json.load(f)
-
-    coins = balance.get(user_id, {}).get("coin", 0)
-    fragments = balance.get(user_id, {}).get("fragments", 0)
-    quartz = balance.get(user_id, {}).get("quartz", 0)
-
-    await ctx.send(f"{member.mention} Coins: {coins}, Fragments: {fragments}, Quartz: {quartz}")
-
-@bot.command()
-async def givecoin(ctx, member: discord.Member, amount: int):
-    user_id = str(ctx.author.id)
-    reveicer = str(member.id)
-
-    if not user_id or not amount:
-        await ctx.send("Missing member or amount `mb!givecoin [@mention] [amount]`")
-    
-    with open(BALANCE_FILE, "r") as f:
-        balance = json.load(f)
-
-    if user_id not in balance:
-        balance[user_id] = {}
-    if "coin" not in balance[user_id]:
-        balance[user_id]["coin"] = 0
-
-    balance[reveicer]["coin"] += amount
-
-    with open(BALANCE_FILE, "w") as f:
-        json.dump(balance, f, indent=4)
-
-    await ctx.send(f"Gave {amount} Coins to {member.mention}")
 
 bot.run(os.environ["TOKEN"])
