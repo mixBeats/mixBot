8b428e49559a2c7c4aca4759183b3b47402b7acb
Atima <atimarity@gmail.com>
2025-09-25 14:12:50 +0300

    Update currency.py

--- cogs/currency.py
+++ cogs/currency.py
@@ -3,8 +3,7 @@ from discord.ext import commands
 class Currency(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print("Running cog")
-
+        
     @commands.command()
     async def hello(self, ctx):
         await ctx.send("Hello!")
