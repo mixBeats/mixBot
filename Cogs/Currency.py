3ec3bb8b5bf3c0ae147cd229f9ffbd1994f0a45d
Atima <atimarity@gmail.com>
2025-09-25 13:23:52 +0300

    Update Currency.py

--- Cogs/Currency.py
+++ Cogs/Currency.py
@@ -4,6 +4,8 @@ class Currency(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    print ("Running cog");
+
     @commands.command()
     async def hello(self, ctx):
         await ctx.send("Hello!")
