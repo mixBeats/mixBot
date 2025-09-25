ee740afe330da99b0df6e1a306dc0853888f8c94
Atima <atimarity@gmail.com>
2025-09-25 12:39:21 +0300

    Update and rename Currency to Currency.py

---
+++ Cogs/Currency.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class Currency(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send("Hello!")
+
+async def setup(bot):
+    await bot.add_cog(General(bot))
