f206360568a7e2ebeb12adb3695463a90f2eaa22
Atima <atimarity@gmail.com>
2025-09-26 16:19:07 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const { token } = require('./config.json');
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === 'mb!hello') {
+        message.channel.send('Hello!');
+    }
+});
+
+client.login(token);
