302a3726e60b9d4bcd58a23fd6fa2bc5721e289d
Atima <atimarity@gmail.com>
2025-09-30 00:11:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 const client = new Client({
   intents: [
