6390a8caa746171dd7499827c71889a68c217010
Atima <atimarity@gmail.com>
2025-09-30 21:01:27 +0300

    Update currency.js

--- commands/currency.js
+++ commands/currency.js
@@ -43,7 +43,7 @@ const addCoinsCommand = {
     await storage.setItem(userId, coins);
 
     await message.channel.send(
-      `Added **${amount}** coins to <@${userId}>**`
+      `Added **${amount}** coins to <@${userId}>`
     );
   },
 };
