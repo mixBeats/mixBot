50f8f9377d4233a2bd6e2442cc926bb665e8e1d8
Atima <atimarity@gmail.com>
2025-10-01 13:56:56 +0300

    Update currency.js

--- commands/currency.js
+++ commands/currency.js
@@ -48,33 +48,4 @@ const addCoinsCommand = {
   },
 };
 
-const topCommand = {
-  name: 'top',
-  description: 'Top leaderboard for currency',
-  async execute(message, args, client) {
-    const data = await storage.values();
-
-    if (!data.length) {
-      return message.channel.send("No users found in the database yet!");
-    }
-
-  const users = data
-      .map(entry => ({
-        userId: entry.userId,
-        coins: entry.coins || 0
-      }))
-      .sort((a, b) => b.coins - a.coins)
-      .slice(0, 10);
-
-    let leaderboard = "**mixBeats currency leaderboard** \n";
-    for(const i = 0; i<users.length; i++){
-      const user = await client.users.fetch(users[i].userId).catch(() => null);
-      const name = user ? user.username : "Unknown User";
-      leaderboard += `${i}. ${user.username} - Coins: ${users[i].coins} \n`;
-    }
-
-    message.channel.send(leaderboard || "No data yet");
-  }
-};
-
-module.exports = [balanceCommand, addCoinsCommand, topCommand];
+module.exports = [balanceCommand, addCoinsCommand];
