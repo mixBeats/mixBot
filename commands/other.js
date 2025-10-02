750772c832c114ae380f53a43830ae101a3b2ab9
Atima <atimarity@gmail.com>
2025-10-03 02:11:19 +0300

    Update other.js

--- commands/other.js
+++ commands/other.js
@@ -2,7 +2,7 @@ const helpCommand = {
   name: 'help',
   description: 'gives help about mixbeat',
   async execute(message, args) {
-    message.channel.send("**mixBot commands** 
+    message.channel.send('**mixBot commands** 
                          \n `Moderation ----`
                          \n `mb!ban` - Bans a user
                          \n `mb!kick` - kicks a user
@@ -17,8 +17,7 @@ const helpCommand = {
                          \n `mb!rank` - Shows your level up
                          \n `mb!lb` - Shows top 10 leaderboard for level up
                          \n `Other ----`
-                         \n `mb!help` - Shows this help command
-    ");
+                         \n `mb!help` - Shows this help command ');
   }
 };
 module.exports = [helpCommand];
