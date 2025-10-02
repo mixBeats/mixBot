f8a4f4ff80c9c793470f8bec78166fcfba9eb3cd
Atima <atimarity@gmail.com>
2025-10-03 02:22:55 +0300

    Update other.js

--- commands/other.js
+++ commands/other.js
@@ -2,22 +2,29 @@ const helpCommand = {
   name: 'help',
   description: 'gives help about mixbeat',
   async execute(message, args) {
-    message.channel.send('**mixBot commands** 
-                         \n `Moderation ----`
-                         \n `mb!ban` - Bans a user
-                         \n `mb!kick` - kicks a user
-                         \n `mb!mute` - Mutes a user for a duration
-                         \n `Currency ----`
-                         \n `mb!add-coins` - Adds coins to a person (Admin only)
-                         \n `mb!remove-coins` - Removes coins to a person (Admin only)
-                         \n `mb!give` - Gives coins to a member
-                         \n `mb!top` - Gives top 10 leaderboard for currency
-                         \n `mb!bal` - Checks for your balance
-                         \n `Level ----`
-                         \n `mb!rank` - Shows your level up
-                         \n `mb!lb` - Shows top 10 leaderboard for level up
-                         \n `Other ----`
-                         \n `mb!help` - Shows this help command ');
+message.channel.send(`**mixBot commands**
+
+Moderation ----
+\`mb!ban\` - Bans a user
+\`mb!kick\` - Kicks a user
+\`mb!mute\` - Mutes a user for a duration
+
+Currency ----
+\`mb!add-coins\` - Adds coins to a person (Admin only)
+\`mb!remove-coins\` - Removes coins from a person (Admin only)
+\`mb!give\` - Gives coins to a member
+\`mb!top\` - Gives top 10 leaderboard for currency
+\`mb!bal\` - Checks for your balance
+
+Level ----
+\`mb!rank\` - Shows your level up
+\`mb!lb\` - Shows top 10 leaderboard for level up
+
+Other ----
+\`mb!help\` - Shows this help command
+`);
+
+
   }
 };
 module.exports = [helpCommand];
