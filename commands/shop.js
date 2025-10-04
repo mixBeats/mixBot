1a1e7fdf8a9bac9e86194f2dff357a1d554181a5
Atima <atimarity@gmail.com>
2025-10-05 01:58:53 +0300

    Create shop.js

---
+++ commands/shop.js
@@ -0,0 +1,24 @@
+const getItems = require('../items');
+
+const shopCommand = {
+  name: 'shop',
+  description: 'shows all available items in the bot',
+
+    async execute(message) {
+      const items = getItems();
+
+      if(items.length === 0){
+        return message.channel.send("The Shop is empty");
+      }
+
+      let reply = "**mixBeats Shop** \n";
+      items.forEach((item, index) => {
+        reply += `${index}. ${item.name} - ${item.price} coins \n ${item description} \n`;
+      });
+
+      message.channel.send(reply);
+      
+    }
+};
+
+module.exports = [shopCommand];
