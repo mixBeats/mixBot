f5027b815408804afc122a0a065fb87b50a41258
Atima <atimarity@gmail.com>
2025-10-05 03:12:43 +0300

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,4 +1,4 @@
-const { addItem, removeItem, getItems } = require('../items');
+const { addItem, removeItem, getItems, editItem } = require('../items');
 
 module.exports = [
   {
@@ -13,7 +13,7 @@ module.exports = [
 
    let reply = "**mixBeats Shop**\n";
     items.forEach((item, index) => {
-      reply += `**${index}. ${item.name}** - ${item.price} coins\n ${item.description}\n`;
+      reply += `**${item.emoji} ${item.name}** - ${item.price} coins\n ${item.description}\n`;
     });
       message.channel.send(reply);
     }
@@ -26,14 +26,14 @@ module.exports = [
     return message.reply("❌ You do not have premissions to add items");
     }
 
-  const [name, price, ...descParts] = args;
+  const [name, emoji, price, ...descParts] = args;
   const description = descParts.join(" ");
 
-  if(!name || !price || !descParts){
-    return message.reply("Use: `mb!add-item <name> <price> <description>`");
+  if(!name || !emoji || !price || !descParts){
+    return message.reply("Use: `mb!add-item <name> <emoji> <price> <description>`");
   }
 
-  addItem(name, parseInt(price), description);
+  addItem(name, emoji, parseInt(price), description);
   message.channel.send(`✅ Successfully made ${name} item`);
     }
   },
@@ -54,6 +54,35 @@ module.exports = [
 
   removeItem(name);
   message.channel.send(`Removed ${name} item`);
+    }
+  },
+
+  {
+  name: 'edit-items',
+  description: 'Edits an item in the shop',
+  async execute(message, args) {
+  if(!message.member.permissions.has("Administrator")){
+    return message.reply("❌ You do not have premissions to edit items");
+    }
+
+    const [name, field, ...valueParts] = args;
+    const newValue = valueParts.join(" ");
+
+    if(!name || !field || !newValue){
+      return message.reply("Use: `mb!edit-items <name OR price OR description>`")
+    }
+
+    const validFields = ['name', 'price', 'description'];
+      if (!validFields.includes(field.toLowerCase())) {
+        return message.reply("❌ You can edit: `name` or `price` or `description`");
+      }
+
+    const success = editItem(name, field, newValue);
+    if(!success){
+      return message.reply("❌ Item not found");
+    }
+    
+    message.channel.send(`Edited ${name} item`);
     }
   }
 ];
