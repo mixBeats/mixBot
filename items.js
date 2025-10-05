c7dc5763ec8f1ab5af07b6be134425665c4a1939
Atima <atimarity@gmail.com>
2025-10-05 03:04:13 +0300

    Update items.js

--- items.js
+++ items.js
@@ -10,8 +10,8 @@ function saveItems() {
   fs.writeFileSync(ITEMS_FILE, JSON.stringify(items, null, 2));
 }
 
-function addItem(name, price, description) {
-  items.push({ name, price, description });
+function addItem(name, emoji, price, description) {
+  items.push({ name, emoji, price, description });
   saveItems();
 }
 
@@ -24,4 +24,20 @@ function getItems() {
   return items;
 }
 
-module.exports = { addItem, removeItem, getItems };
+function editItem(name, field, newValue) {
+  const item = items.find(i => i.name.toLowerCase() === name.toLowerCase());
+  if (!item) return false;
+
+  if (field === 'price') {
+    const parsed = parseInt(newValue);
+    if (isNaN(parsed)) return false;
+    item.price = parsed;
+  } else {
+    item[field] = newValue;
+  }
+
+  saveItems();
+  return true;
+}
+
+module.exports = { addItem, removeItem, getItems, editItem };
