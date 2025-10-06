4f255b56b5e3999f7ebaa5b369065cd0c3115ed5
Atima <atimarity@gmail.com>
2025-10-06 06:43:56 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1873SWvVV7BVtMsiOgHn2BhavQwx4D8o",
+  authDomain: "mixbeats-da143.firebaseapp.com",
+  databaseURL: "https://mixbeats-da143-default-rtdb.firebaseio.com",
+  projectId: "mixbeats-da143",
+  storageBucket: "mixbeats-da143.firebasestorage.app",
+  messagingSenderId: "234984118517",
+  appId: "1:234984118517:web:b5837eb622796a0be0cdbc",
+  measurementId: "G-QJRWEKG97W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
