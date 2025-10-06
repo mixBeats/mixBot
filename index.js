2c164542c890518e7d46624d65ac6374a4cae380
Atima <atimarity@gmail.com>
2025-10-06 07:43:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
+const { db } = require('./firebase');
 
 const client = new Client({
   intents: [
